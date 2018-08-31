f747fbd69897d90f15d6f50af9790e695f2f9c31
leesungrin <leesungrin1@gmail.com>
2018-08-31 23:15:12 +1000

    enable update profile activity to direct to main activity

--- SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Activities/UpdateProfileActivity.java
+++ SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Activities/UpdateProfileActivity.java
@@ -1,15 +1,40 @@
 package yourteamnumber.seshealthpatient.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import yourteamnumber.seshealthpatient.R;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 
+    final static int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+        final Spinner spinner = (Spinner) findViewById(R.id.patient_gender_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        Button update_btn = findViewById(R.id.update_profile_button);
+
+        update_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UpdateProfileActivity.this, MainActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
     }
 }
