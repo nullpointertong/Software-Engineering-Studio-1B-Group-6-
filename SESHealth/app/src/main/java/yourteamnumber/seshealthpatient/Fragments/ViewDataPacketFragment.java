b4f77d1a6c5a3c3f951ab71f4b363bf0fc8ec13c
Duc Vu <vutheduc96@gmail.com>
2018-09-28 17:15:21 +1000

    Update data packet view

--- SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Fragments/ViewDataPacketFragment.java
+++ SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Fragments/ViewDataPacketFragment.java
@@ -2,6 +2,8 @@ package yourteamnumber.seshealthpatient.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +20,15 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 import yourteamnumber.seshealthpatient.Model.DataPacket.Models.DataPacket;
 import yourteamnumber.seshealthpatient.Model.DataPacket.Models.Location;
@@ -29,6 +40,7 @@ public class ViewDataPacketFragment extends Fragment {
     private MapView map;
     private GoogleMap mMap;
     private String patientName = null;
+    private StorageReference storageRef;
 
     public ViewDataPacketFragment() {
         // Required empty public constructor
@@ -41,12 +53,14 @@ public class ViewDataPacketFragment extends Fragment {
             if (getArguments().getSerializable("data_packet") != null) {
                 dataPacket = (DataPacket) getArguments().getSerializable("data_packet");
             } else {
-                Toast.makeText(this.getContext(), "Error: Data packet cannot be read", Toast.LENGTH_SHORT);
+                Toast.makeText(this.getContext(), "Error: Data packet read error", Toast.LENGTH_SHORT);
                 this.getFragmentManager().popBackStackImmediate();
             }
 
             patientName = getArguments().getString("patient_name");
         }
+
+        storageRef = FirebaseStorage.getInstance().getReference();
 
     }
 
@@ -62,12 +76,10 @@ public class ViewDataPacketFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         /** Display patient name on top of the packet (if doctor side) **/
-        if (patientName != null) {
+        if (patientName != null)
             ((TextView)getActivity().findViewById(R.id.patient_name)).setText(patientName);
-        }
-        else {
+        else
             getActivity().findViewById(R.id.patient_name).setVisibility(View.GONE);
-        }
         /**
           Display the information from the patient's data packet
           **/
@@ -76,34 +88,51 @@ public class ViewDataPacketFragment extends Fragment {
         ((TextView)getActivity().findViewById(R.id.data_packet_desc_tv))
                 .setText(dataPacket.getTextData() != null ? dataPacket.getTextData().getData() : "N.A.");
         ((TextView)getActivity().findViewById(R.id.data_packet_location_tv))
-                .setText(dataPacket.getLocation() != null ? dataPacket.getLocation().toString() : "N.A.");
+                .setText(dataPacket.getLocation() != null ? "" : "N.A.");
+
         if (dataPacket.getLocation() != null)
             /** Show the patient location in a map container **/
             showLocation(view, savedInstanceState, dataPacket.getLocation());
         else
             /** If location not available, Hide the map container **/
             getActivity().findViewById(R.id.mapContainer).setVisibility(View.GONE);
+
         ((TextView)getActivity().findViewById(R.id.data_packet_heartrate_tv))
                 .setText(dataPacket.getHeartRate() != null ? dataPacket.getHeartRate().toString() : "N.A.");
         Button videoSnippetBtn = ((Button)getActivity().findViewById(R.id.data_packet_video_button));
         if (dataPacket.getVideoSnippet() != null) {
             videoSnippetBtn.setText("DOWNLOAD");
             videoSnippetBtn.setClickable(true);
+            videoSnippetBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    StorageReference videoRef = storageRef.child(dataPacket.getDataPackedId().toString()).child("videos");
+                    //downloadFile(videoRef);
+
+                }
+            });
         }
         Button suppFilesBtn = ((Button)getActivity().findViewById(R.id.data_packet_file_button));
         if (dataPacket.getSupplementaryFiles() != null) {
             suppFilesBtn.setText("DOWNLOAD");
-            videoSnippetBtn.setClickable(true);
+            suppFilesBtn.setClickable(true);
+            suppFilesBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
         }
 
     }
 
     private void showLocation(View view, Bundle savedInstanceState, Location location)
     {
+        /** Show the location of the patient shared in the data packet in a map container **/
         map = view.findViewById(R.id.mapViewPatient);
         map.onCreate(savedInstanceState);
 
-        map.onResume(); // needed to get the map to display immediately
+        map.onResume(); /** needed to get the map to display immediately **/
 
         map.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -115,6 +144,29 @@ public class ViewDataPacketFragment extends Fragment {
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
                 mMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        });
+    }
+
+    private void downloadFile(StorageReference storageRef) {
+
+        File rootPath = new File(Environment.getExternalStorageDirectory(), "imageName.txt");
+        if(!rootPath.exists()) {
+            rootPath.mkdirs();
+        }
+
+        final File localFile = new File(rootPath,"imageName.txt");
+
+        storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Log.e("firebase ",";local temp file created" +localFile.toString());
+                //  updateDb(timestamp,localFile.toString(),position);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e("firebase ",";local temp file not created" +exception.toString());
             }
         });
     }
